35f0f8200a9c002a4a507cf85955f5ff37fc1627
danielradu <daniel.radu@student.tuiasi.ro>
2026-04-03 21:48:13 +0300

    unit tests

--- mempool/utils.go
+++ mempool/utils.go
@@ -5,11 +5,27 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/tiktoken-go/tokenizer"
 
 	"moa-chain/testscommon"
 )
 
 var expectedError = errors.New("error expected")
+
+func newTestMemPool() *memPool {
+	mp := NewMemPool()
+	mp.tokensConfig = map[string]uint64{
+		"short":         0,
+		"medium":        10,
+		"long":          15,
+		"fast":          10,
+		"standard":      15,
+		"thinking":      1985,
+		"deep research": 8985,
+	}
+
+	return mp
+}
 
 func createTx(nonce uint64, estimatedConsumption uint64, txHash []byte) *transaction {
 	return &transaction{
@@ -42,13 +58,71 @@ func createSelectionTx(
 	txHash []byte,
 ) *transaction {
 	return &transaction{
-		nonce:                nonce,
-		sender:               []byte(sender),
-		estimatedScore:       estimatedScore,
-		estimatedConsumption: estimatedConsumption,
-		transferredValue:     transferredValue,
-		txHash:               txHash,
+		nonce:               nonce,
+		prompt:              []byte{},
+		sender:              []byte(sender),
+		transferredValue:    transferredValue,
+		tip:                 estimatedScore * estimatedConsumption,
+		txHash:              txHash,
+		userOutputDimension: consumptionToOutputDimension(estimatedConsumption),
+		thinkingMode:        consumptionToThinkingMode(estimatedConsumption),
 	}
+}
+
+func createPoolTx(
+	nonce uint64,
+	sender string,
+	estimatedConsumption uint64,
+	txHash []byte,
+) *transaction {
+	return &transaction{
+		nonce:               nonce,
+		prompt:              []byte{},
+		sender:              []byte(sender),
+		txHash:              txHash,
+		userOutputDimension: consumptionToOutputDimension(estimatedConsumption),
+		thinkingMode:        consumptionToThinkingMode(estimatedConsumption),
+	}
+}
+
+func consumptionToOutputDimension(estimatedConsumption uint64) string {
+	switch estimatedConsumption {
+	case 10:
+		return "short"
+	case 20:
+		return "medium"
+	case 15, 30, 2000, 9000:
+		return "long"
+	default:
+		return "short"
+	}
+}
+
+func consumptionToThinkingMode(estimatedConsumption uint64) string {
+	switch estimatedConsumption {
+	case 10, 20, 25:
+		return "fast"
+	case 15, 30:
+		return "standard"
+	case 2000:
+		return "thinking"
+	case 9000:
+		return "deep research"
+	default:
+		return "fast"
+	}
+}
+
+func countPromptTokensForTest(t *testing.T, prompt string) uint64 {
+	t.Helper()
+
+	enc, err := tokenizer.Get(tokenizer.Cl100kBase)
+	require.NoError(t, err)
+
+	numTokens, err := enc.Count(prompt)
+	require.NoError(t, err)
+
+	return uint64(numTokens)
 }
 
 func createAccountsStateWithAccounts(t *testing.T, accounts map[string]struct {
